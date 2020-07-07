e1ee4781a2c90cff0fdadb849a6d1228184a1613
EthanFreestone <ethan.freestone@k-int.co.uk>
2020-07-07 17:37:16 +0100

    added test for comparisonInfo component

---
+++ test/bigtest/interactors/comparisons-info.js
@@ -0,0 +1,27 @@
+import {
+  clickable,
+  interactor,
+  isPresent,
+  text
+} from '@bigtest/interactor';
+
+
+export default @interactor class ComparisonsInfoInteractor {
+  comparisonName = text('[data-test-comparison-name]');
+  comparisonStatus = text('[data-test-comparison-status]');
+
+  isComparisonResultPresent = isPresent('[data-test-comparison-result]');
+  comparisonResult = text('[data-test-comparison-result]');
+
+  isComparisonErrorsPresent = isPresent('[data-test-comparison-errors]');
+  comparisonErrors = text('[data-test-comparison-errors]');
+
+  isComparisonStartedPresent = isPresent('[data-test-comparison-started]');
+  comparisonStarted = text('[data-test-comparison-started]');
+
+  isComparisonEndedPresent = isPresent('[data-test-comparison-ended]');
+  comparisonEnded = text('[data-test-comparison-ended]');
+
+  isViewReportButtonPresent = isPresent('[data-test-comparison-report-view]');
+  viewReportButton = clickable('[data-test-comparison-report-view]');
+}
