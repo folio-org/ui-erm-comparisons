4a1c10a70bf05327505407964358eea5d0d7c969
aditya matukumalli <aditya.matukumalli@gmail.com>
2020-07-29 15:43:49 -0400

    Improve efficient and rendering time for comparison report, refs ERM-1032 (#35)
    - Pagination
    - Use the load more option to fetch limited results
    - translations

--- src/components/views/ComparisonReport.js
+++ src/components/views/ComparisonReport.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { FormattedMessage } from 'react-intl';
 import {
+  Button,
   IconButton,
+  Layout,
   LoadingPane,
   Pane,
   PaneMenu,
@@ -13,16 +15,11 @@ import { AppIcon } from '@folio/stripes/core';
 
 import ComparisonReportList from './ComparisonReportList';
 
-export default class ComparisonReport extends React.Component {
-  static propTypes = {
-    data: PropTypes.shape({
-      comparisonPointData: PropTypes.object,
-    }),
-    isLoading: PropTypes.bool,
-    onClose: PropTypes.func.isRequired
-  }
+const resourceCount = 100;
+const ComparisonReport = ({ data, isLoading, onClose }) => {
+  const [currentIndex, setCurrentIndex] = useState(resourceCount);
 
-  renderFirstMenu() {
+  const renderFirstMenu = () => {
     return (
       <PaneMenu>
         <FormattedMessage id="ui-erm-comparisons.report.close">
@@ -31,36 +28,69 @@ export default class ComparisonReport extends React.Component {
               aria-label={ariaLabel}
               icon="times"
               id="close-report-button"
-              onClick={this.props.onClose}
+              onClick={onClose}
             />
           )}
         </FormattedMessage>
       </PaneMenu>
     );
-  }
+  };
 
-  render() {
-    const paneProps = {
-      defaultWidth: 'fill',
-      id: 'pane-report',
-      onClose: this.props.onClose,
-    };
+  const handleNeedMoreData = () => {
+    setCurrentIndex(currentIndex + resourceCount);
+  };
 
-    if (this.props.isLoading) return <LoadingPane data-loading {...paneProps} />;
+  const paneProps = {
+    defaultWidth: 'fill',
+    id: 'pane-report',
+    onClose,
+  };
 
-    const { comparisonPointData } = this.props.data;
+  if (isLoading) return <LoadingPane data-loading {...paneProps} />;
 
-    return (
-      <Paneset>
-        <Pane
-          appIcon={<AppIcon app="erm-comparisons" />}
-          firstMenu={this.renderFirstMenu()}
-          paneTitle={comparisonPointData?.name}
-          {...paneProps}
-        >
-          <ComparisonReportList sourceData={this.props.data} />
-        </Pane>
-      </Paneset>
-    );
-  }
-}
+  const { comparisonPointData } = data;
+  const currentResources = data?.report?.slice(0, currentIndex);
+  const totalCount = data?.report?.length ?? 0;
+
+  return (
+    <Paneset>
+      <Pane
+        appIcon={<AppIcon app="erm-comparisons" />}
+        firstMenu={renderFirstMenu()}
+        paneTitle={comparisonPointData?.name}
+        {...paneProps}
+      >
+        <div>
+          <ComparisonReportList
+            sourceData={{ comparisonPointData, report: currentResources }}
+            totalCount={totalCount}
+          />
+          {
+            currentResources.length < totalCount && (
+              <Layout className="marginTop1 textCentered">
+                <Button
+                  onClick={handleNeedMoreData}
+                  style={{ width: '75%' }}
+                >
+                  <FormattedMessage id="ui-erm-comparisons.comparisonReport.loadMore" />
+                </Button>
+              </Layout>
+            )
+          }
+        </div>
+      </Pane>
+    </Paneset>
+  );
+};
+
+ComparisonReport.propTypes = {
+  data: PropTypes.shape({
+    comparisonPointData: PropTypes.object,
+  }),
+  isLoading: PropTypes.bool,
+  onClose: PropTypes.func.isRequired
+};
+
+export default ComparisonReport;
+
+
