de58982d048627397f9fd80210219b7c75b62956
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-11-10 15:16:29 +0000

    test: erm-testing
    Moved all jest tests over to manual mocks and imports from stripes-erm-testing

--- src/components/ComparisonFilters/ComparisonPointFilter.test.js
+++ src/components/ComparisonFilters/ComparisonPointFilter.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import '@folio/stripes-erm-components/test/jest/__mock__';
-import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { renderWithIntl } from '@folio/stripes-erm-testing';
 import { MemoryRouter } from 'react-router-dom';
-import translationsProperties from '../../../test/bigtest/helpers/translationsProperties';
+import translationsProperties from '../../../test/jest/helpers/translationsProperties';
 import ComparisonPointFilter from './ComparisonPointFilter';
 
 const onAgreementSelected = jest.fn();
---
+++ test/jest/helpers/translationsProperties.js
@@ -0,0 +1,21 @@
+import { translationsProperties as coreTranslations } from '@folio/stripes-erm-testing';
+
+// Direct import is a bit gross, but so is exposing the translations file...
+// no super great way to do this so this will do for now.
+import ermTranslations from '@folio/stripes-erm-components/translations/stripes-erm-components/en.json';
+
+import translations from '../../../translations/ui-erm-comparisons/en';
+
+const translationsProperties = [
+  {
+    prefix: 'ui-erm-comparisons',
+    translations,
+  },
+  {
+    prefix: 'stripes-erm-components',
+    translations: ermTranslations
+  },
+  ...coreTranslations
+];
+
+export default translationsProperties;
