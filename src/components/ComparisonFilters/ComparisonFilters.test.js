a2a5e35538bd2af7a17d8ee92599d19b4ce50991
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-11-10 15:04:30 +0000

    ERM-1307, Add test coverage for ComparisonFilters (#173)
    * ERM-1307, Add test coverage for ComparisonFilters
    
    * add jest-environment-jsdom to package.json file
    
    Co-authored-by: EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>

---
+++ src/components/ComparisonFilters/ComparisonFilters.test.js
@@ -0,0 +1,178 @@
+import React from 'react';
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { waitFor } from '@testing-library/dom';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { Accordion, Checkbox } from '@folio/stripes-testing';
+import { MemoryRouter } from 'react-router-dom';
+import translationsProperties from '../../../test/bigtest/helpers/translationsProperties';
+import ComparisonFilters from './ComparisonFilters';
+
+jest.mock('./ComparisonPointFilter', () => () => <div>ComparisonPointFilter</div>);
+const activeFilters = {
+  'comparisonPointOne': [
+    '8a029ec3-f55d-46ea-9d04-5cdf2fd4a3ed'
+  ]
+};
+
+const data = {
+  'comparisons': [{
+    'id': 'e5c0ef54-fc95-4333-b539-96c7b0bd8630',
+    'dateCreated': 1667815696497,
+    'name': 'test 1',
+    'runnerId': '8d8ee494-a603-42b3-8e47-617bce1acd15',
+    'comparisonPoints': [{
+      'id': '06f886e1-d924-41d4-906c-59ce69f2a7a3',
+      'date': '2022-11-02',
+      'job': {
+        'id': 'e5c0ef54-fc95-4333-b539-96c7b0bd8630'
+      },
+      'titleList': {
+        'id': '8a029ec3-f55d-46ea-9d04-5cdf2fd4a3ed',
+        'name': 'MR Agreement',
+        'class': 'org.olf.erm.SubscriptionAgreement'
+      }
+    },
+    {
+      'id': 'eccdaf8f-2d4f-4906-90c9-2a61fd8cf4aa',
+      'date': '2022-11-07',
+      'job': {
+        'id': 'e5c0ef54-fc95-4333-b539-96c7b0bd8630'
+      },
+      'titleList': {
+        'id': 'fb1be09a-e304-465d-8c40-33a2449bcdb4',
+        'name': 'ACM Digtal Library',
+        'class': 'org.olf.kb.Pkg'
+      }
+    }
+    ],
+    'started': 1667815718438,
+    'status': {
+      'id': '2c91809c844d2d8c01844d3592c20047',
+      'value': 'in_progress',
+      'label': 'In progress'
+    },
+    'class': 'org.olf.general.jobs.ComparisonJob',
+    'fullLogCount': 0,
+    'errorLogCount': 0,
+    'infoLogCount': 0
+  }],
+  'resultValues': [{
+    'id': '2c91809c844d2d8c01844d3592ae0041',
+    'value': 'success',
+    'label': 'Success'
+  },
+  {
+    'id': '2c91809c844d2d8c01844d3592b20042',
+    'value': 'partial_success',
+    'label': 'Partial success'
+  },
+  {
+    'id': '2c91809c844d2d8c01844d3592b60043',
+    'value': 'failure',
+    'label': 'Failure'
+  },
+  {
+    'id': '2c91809c844d2d8c01844d3592ba0044',
+    'value': 'interrupted',
+    'label': 'Interrupted'
+  }
+  ],
+  'statusValues': [{
+    'id': '2c91809c843dbb2701843dc312f50034',
+    'value': 'queued',
+    'label': 'Queued'
+  },
+  {
+    'id': '2c91809c843dbb2701843dc312f80035',
+    'value': 'in_progress',
+    'label': 'In progress'
+  },
+  {
+    'id': '2c91809c843dbb2701843dc312fc0036',
+    'value': 'ended',
+    'label': 'Ended'
+  }
+  ]
+};
+
+const stateMock = jest.fn();
+const filterHandlers = {
+  state: stateMock,
+  'checkbox': () => {},
+  'clear': () => {},
+  'clearGroup': () => {},
+  'reset': () => {}
+};
+
+describe('ComparisonFilters', () => {
+  let renderComponent;
+  beforeEach(() => {
+    renderComponent = renderWithIntl(
+      <MemoryRouter>
+        <ComparisonFilters
+          activeFilters={activeFilters}
+          data={data}
+          filterHandlers={filterHandlers}
+        />
+      </MemoryRouter>,
+      translationsProperties
+    );
+  });
+
+  test('renders the Running status Accordion', async () => {
+    await Accordion('Running status').exists();
+  });
+
+  test('renders the Result Accordion', async () => {
+    await Accordion('Result').exists();
+  });
+
+  test('renders the Comparison point 1 Accordion', async () => {
+    await Accordion('Comparison point 1').exists();
+  });
+
+  test('renders the Comparison point 2 Accordion', async () => {
+    await Accordion('Comparison point 2').exists();
+  });
+
+  it('renders ComparisonPointFilter component', () => {
+    const { getAllByText } = renderComponent;
+    expect(getAllByText('ComparisonPointFilter')).toBeDefined();
+  });
+
+  test('renders the expected button name', () => {
+    const { getByRole } = renderComponent;
+    expect(getByRole('button', { name: 'Comparison point 1 filter list' }));
+  });
+
+  test('renders the expected button name', () => {
+    const { getByRole } = renderComponent;
+    expect(getByRole('button', { name: 'Comparison point 2 filter list' }));
+  });
+
+  let index = 1;
+  const testComparisonsFilterCheckbox = (field, value) => (
+    test(`clicking the ${value} checkbox`, async () => {
+      await waitFor(async () => {
+        await Checkbox({ id: `clickable-filter-${field}-${value}` }).click();
+      });
+
+      await waitFor(() => {
+        expect(stateMock.mock.calls.length).toEqual(index);
+      });
+
+      index++;
+    })
+  );
+
+  // Running status checkboxes
+  testComparisonsFilterCheckbox('status', 'queued');
+  testComparisonsFilterCheckbox('status', 'in-progress');
+  testComparisonsFilterCheckbox('status', 'ended');
+
+  // Result checkboxes
+  testComparisonsFilterCheckbox('result', 'success');
+  testComparisonsFilterCheckbox('result', 'partial-success');
+  testComparisonsFilterCheckbox('result', 'failure');
+  testComparisonsFilterCheckbox('result', 'interrupted');
+});
--- src/components/ComparisonFilters/ComparisonPointFilter.js
+++ src/components/ComparisonFilters/ComparisonPointFilter.js
@@ -1,88 +1,91 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { Button, Layout } from '@folio/stripes/components';
 import { Pluggable } from '@folio/stripes/core';
 
-class ComparisonPointFilter extends React.Component {
-  static propTypes = {
-    disabled: PropTypes.bool,
-    name: PropTypes.string,
-    onAgreementSelected: PropTypes.func.isRequired,
-    onPackageSelected: PropTypes.func.isRequired,
+const ComparisonPointFilter = ({ disabled, name, onAgreementSelected, onPackageSelected }) => {
+  let triggerButton = useRef(null);
+  const renderFilterAgreementButton = () => {
+    return (
+      <div data-testid="selectAgreementPluggin">
+        <Pluggable
+          dataKey={`agreement-${name}`}
+          onAgreementSelected={onAgreementSelected}
+          renderTrigger={(pluggableRenderProps) => {
+            triggerButton = pluggableRenderProps.buttonRef;
+            const buttonProps = {
+              'aria-haspopup': 'true',
+              'buttonRef': triggerButton,
+              'id': `${name}-agreement-search-button`,
+              'onClick': pluggableRenderProps.onClick,
+            };
+            return (
+              <Button
+                key={`data-test-add-${name}-agreement-filter`}
+                disabled={disabled}
+                {...buttonProps}
+              >
+                <FormattedMessage id="ui-erm-comparisons.selectAgreement" />
+              </Button>
+            );
+          }}
+          type="find-agreement"
+        >
+          <FormattedMessage id="ui-erm-comparisons.newComparison.noAgreementPlugin" />
+        </Pluggable>
+      </div>
+    );
   };
 
-  renderFilterAgreementButton = () => {
+  const renderFilterPackageButton = () => {
     return (
-      <Pluggable
-        dataKey={`agreement-${this.props.name}`}
-        onAgreementSelected={this.props.onAgreementSelected}
-        renderTrigger={(props) => {
-          this.triggerButton = props.buttonRef;
-          const buttonProps = {
-            'aria-haspopup': 'true',
-            'buttonRef': this.triggerButton,
-            'id': `${this.props.name}-agreement-search-button`,
-            'onClick': props.onClick,
-          };
-          return (
-            <Button
-              key={`data-test-add-${this.props.name}-agreement-filter`}
-              disabled={this.props.disabled}
-              {...buttonProps}
-            >
-              <FormattedMessage id="ui-erm-comparisons.selectAgreement" />
-            </Button>
-          );
-        }}
-        type="find-agreement"
-      >
-        <FormattedMessage id="ui-erm-comparisons.newComparison.noAgreementPlugin" />
-      </Pluggable>
+      <div data-testid="selectPackagePluggin">
+        <Pluggable
+          dataKey={`package-${name}`}
+          onEresourceSelected={onPackageSelected}
+          renderTrigger={(pluggableRenderProps) => {
+            triggerButton = pluggableRenderProps.buttonRef;
+            const buttonProps = {
+              'aria-haspopup': 'true',
+              'buttonRef': triggerButton,
+              'id': `${name}-package-search-button`,
+              'onClick': pluggableRenderProps.onClick,
+            };
+            return (
+              <Button
+                key={`data-test-add-${name}-package-filter`}
+                disabled={disabled}
+                {...buttonProps}
+              >
+                <FormattedMessage id="ui-erm-comparisons.selectPackage" />
+              </Button>
+            );
+          }}
+          showTitles={false}
+          type="find-eresource"
+        >
+          <FormattedMessage id="ui-erm-comparisons.newComparison.noPackagePlugin" />
+        </Pluggable>
+      </div>
     );
-  }
+  };
 
-  renderFilterPackageButton = () => {
-    return (
-      <Pluggable
-        dataKey={`package-${this.props.name}`}
-        onEresourceSelected={this.props.onPackageSelected}
-        renderTrigger={(props) => {
-          this.triggerButton = props.buttonRef;
-          const buttonProps = {
-            'aria-haspopup': 'true',
-            'buttonRef': this.triggerButton,
-            'id': `${this.props.name}-package-search-button`,
-            'onClick': props.onClick,
-          };
-          return (
-            <Button
-              key={`data-test-add-${this.props.name}-package-filter`}
-              disabled={this.props.disabled}
-              {...buttonProps}
-            >
-              <FormattedMessage id="ui-erm-comparisons.selectPackage" />
-            </Button>
-          );
-        }}
-        showTitles={false}
-        type="find-eresource"
-      >
-        <FormattedMessage id="ui-erm-comparisons.newComparison.noPackagePlugin" />
-      </Pluggable>
-    );
-  }
+  return (
+    <>
+      <Layout className="flex-direction-column">
+        {renderFilterAgreementButton()}
+        {renderFilterPackageButton()}
+      </Layout>
+    </>
+  );
+};
 
-  render() {
-    return (
-      <>
-        <Layout className="flex-direction-column">
-          {this.renderFilterAgreementButton()}
-          {this.renderFilterPackageButton()}
-        </Layout>
-      </>
-    );
-  }
-}
+ComparisonPointFilter.propTypes = {
+  disabled: PropTypes.bool,
+  name: PropTypes.string,
+  onAgreementSelected: PropTypes.func.isRequired,
+  onPackageSelected: PropTypes.func.isRequired,
+};
 
 export default ComparisonPointFilter;
