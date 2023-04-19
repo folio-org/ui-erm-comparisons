40a5018cf12dfe2c1ea707faf2583214d8cfd53c
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2023-04-19 14:56:36 +0200

    ERM-2913 Remove top double-border in split coverage (and subsequent) cells
    * apply css class conditionally if rowIndex !== 0, refs ERM-2907

--- src/components/views/ComparisonReportList/ComparisonReportList.js
+++ src/components/views/ComparisonReportList/ComparisonReportList.js
@@ -315,7 +315,7 @@ const ComparisonReportList = (
                                 return (
                                   <div
                                     key={`row-${rowIndex}`}
-                                    className={`${rowClass} ${css.borderCell}`}
+                                    className={`${rowClass} ${rowIndex !== 0 ? css.borderCell : ''}`}
                                     data-test-coverage-mcl-row
                                     {...rowProps}
                                     // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
